8deea9daff2a1a3c103e15a5a6545ab00bdee9b1
Matt Mills <mmills@2bn.net>
2022-04-20 09:36:36 -0600

    Add bootstrap-icons, tweak spacing and header links

--- header.php
+++ header.php
@@ -16,6 +16,8 @@
 	<title>StellarStellaris - <?=$titles[$this_page]?></title>
 	<meta name="viewport" content="width=device-width">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	<link href="css/card-deck.css" rel="stylesheet">
 </head>
 
 <body>
@@ -25,12 +27,13 @@
 			        <span class="fs-4">StellarStellaris</span>
 			</a>
 			<ul class="nav nav-pills">
-				<li class="nav-item"><a href="//dev.stellarstellaris.com/" class="nav-link <?= ($this_page == 'home')  ? 'active' : ''?>" aria-current="page">Home</a></li>
-				<li class="nav-item"><a href="//mods.dev.stellarstellaris.com/" class="nav-link <?= ($this_page == 'mods')  ? 'active' : ''?>" aria-current="page">Mod DB</a></li>
+				<li class="nav-item"><a href="//dev.stellarstellaris.com/"       class="nav-link <?= ($this_page == 'home')  ? 'active' : ''?>" aria-current="page">Home</a></li>
+				<li class="nav-item"><a href="//mods.dev.stellarstellaris.com/"  class="nav-link <?= ($this_page == 'mods')  ? 'active' : ''?>" aria-current="page">Mod DB</a></li>
 				<li class="nav-item"><a href="//saves.dev.stellarstellaris.com/" class="nav-link <?= ($this_page == 'saves')  ? 'active' : ''?>">Save Editor</a></li>
-				<li class="nav-item"><a href="#" class="nav-link disabled">Code search</a></li>
+				<li class="nav-item"><a href="//code.dev.stellarstellaris.com/"  class="nav-link disabled <?= ($this_page == 'code')  ? 'active' : ''?>">Code search</a></li>
+				<li class="nav-item"><a href="#"                                 class="nav-link disabled <?= ($this_page == 'about')  ? 'active' : ''?>">About</a></li>
 				<li class="nav-item"><a href="https://github.com/MattMills/stellarstellaris" class="nav-link">Github</a></li>
-				<li class="nav-item"><a href="#" class="nav-link disabled <?= ($this_page == 'about')  ? 'active' : ''?>">About</a></li>
+				<li class="nav-item"><a href="https://discord.gg/Mtm5nURDSC" class="nav-link">Discord</a></li>
 			</ul>
 		</header>
 
